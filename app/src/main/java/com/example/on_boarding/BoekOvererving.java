2429c5e14c9d02d6d98499c7ef88f44c24cfa0a7
2147Code <56018106+2147Code@users.noreply.github.com>
2020-05-16 20:09:32 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/on_boarding/BoekOvererving.java
@@ -0,0 +1,61 @@
+package com.example.on_boarding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class BoekOvererving {
+}
+    //connectie maken met de JSON
+    public String getOpleidingUrl (int opleidingnr) throws IOException, JSONException {
+        URL url = new URL("https://adaonboarding.ml/t4/GetBoekenOpleiding?opleiding=" + opleidingnr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        int responseCode = conn.getResponseCode();
+        if(responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        }
+        else  // Hier lees je je json uit die je ervoor hebt opgehaald via je API call
+        {
+            Scanner sc = new Scanner(url.openStream());
+            String inline = "";
+            while(sc.hasNext()) {
+                inline+=sc.nextLine();
+            }
+            sc.close();
+
+            //Je json omzetten naar een object om zo het specifieke ding er uit te halen wat hier dus opleiding id is
+            JSONObject jobj = new JSONObject(inline);
+            JSONArray jsonarr_1 = (JSONArray) jobj.get("item");
+            JSONObject jsonobj_1 = (JSONObject)jsonarr_1.get(0);
+            String opleidingId = jsonobj_1.get("opleiding_id").toString();
+
+            //switch zodat elke gebruiker iets anders ziet ... links nog geen return gegeven. Uitzoeken hoe dit moet
+            switch(opleidingId)
+            {
+                case "1":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079169/Ad-Informatica-leerjaar-1";
+                case "2":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079170/Ad-Accountancy-leerjaar-1";
+                case "3":
+                    return ": https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079174/Ad-Bedrijfskunde-leerjaar-1-";
+                case "4":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL088447/Built-Environment-leerjaar-1";
+                case "5":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079173/Ad-Engineering-leerjaar-1";
+                case "6":
+                    return "https://www.studystore.nl/";
+                case "7":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079166/Ad-Human-Resource-Management-leerjaar-1";
+                case "8":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079171/Ad-Logistiek-leerjaar-1";
+                case "9":
+                    return "https://www.studystore.nl/boekenlijst/Associate-degrees-Academie/2019/BL079172/Ad-Management-leerjaar-1";
+                default:
+                    return "";
