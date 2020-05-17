70366ad16b4ad9aa6bbdd84b4e449d2d7d0693e1
Stan <stanbegthel@gmail.com>
2020-05-17 15:01:26 +0200

    Made inheritance, fixed some shit for resit

--- app/src/main/java/com/example/on_boarding/LastScreenActivity.java
+++ app/src/main/java/com/example/on_boarding/LastScreenActivity.java
@@ -2,30 +2,22 @@ package com.example.on_boarding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
+import com.example.on_boarding.controller.studentController;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+public class LastScreenActivity extends AppCompatActivity {
 
-import java.util.ArrayList;
-import java.util.Date;
+    private studentController ctrlStudent;
 
-public class LastScreenActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+    private TextView tvAfsluiting;
+    private TextView txtTip;
+    private ImageView imageView;
 
-    TextView txtAfsluiting;
-    TextView txtAfsluitingText;
-    TextView txtTip;
-    ImageView imageView;
-    VolleyHelper helper;
-
-    private ArrayList<Student> students;
 
 
     @Override
@@ -33,54 +25,23 @@ public class LastScreenActivity extends AppCompatActivity implements Response.Li
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_screen);
 
-        txtAfsluiting = findViewById(R.id.textView);
         imageView = findViewById(R.id.imageView);
-        txtAfsluitingText = findViewById(R.id.txtAfsluiting);
-        txtTip = findViewById(R.id.textView2);
-        students = new ArrayList<>();
+        txtTip = findViewById(R.id.tvWeetje);
+
+        Intent mainIntent = getIntent();
+        Bundle bundle = mainIntent.getExtras();
+        int iStudentnummer = bundle.getInt("student");
 
 
-        helper = new VolleyHelper(this, "http://192.168.8.15/bp3api");
+        // Student gegevens ophalen
+        tvAfsluiting = findViewById(R.id.tvAfsluitingTekst);
 
-        helper.get("GetStudent?studentnummer=2", null, this, this);
+        // Student controller aanspreken
+        this.ctrlStudent = new studentController(getBaseContext(), tvAfsluiting, 6, iStudentnummer);
 
+        // Student ophalen via de api
+        ctrlStudent.getStudent();
     }
 
-    @Override
-    public void onErrorResponse(VolleyError error) {
-        Log.e("Volley Error", error.toString());
-    }
 
-    @Override
-    public void onResponse(JSONObject response) {
-        System.out.println(response.toString());
-
-        try {
-            students.add(new Student(response));
-            // JSONArray ophalen
-//            JSONArray array = response.getJSONArray("item");
-//
-//            // Kijken of deze leeg is, zodat er geen error onstaat bij geven feedback of gegevens correct
-//            if(array != null && array.length() > 0 ) {
-//                for (int i = 0; i < array.length(); i++) {
-//                    students.add(new Student(array.getJSONObject(i)));
-//                }
-
-                    Log.d("TussenLog", students.toString());
-
-
-                // Door de array lopen
-                for (Student StudentArray : students) {
-                    System.out.println(StudentArray.toString());
-                    // Teksvelden vullen met gegevens student
-                    String name = StudentArray.getStudentNaam() + " " + StudentArray.getStudentAchternaam();
-                    String date = StudentArray.getStartDatum();
-                    txtAfsluitingText.setText("Gefeliciteerd " + name +"! Je hebt bij deze je onboarding afgerond! Je opleiding start op " + date + "! Tot dan!");
-
-                }
-
-        }catch (JSONException e){
-            System.out.println(e);
-        }
-    }
 }
---
+++ app/src/main/java/com/example/on_boarding/helper/Persoon.java
@@ -0,0 +1,7 @@
+package com.example.on_boarding.helper;
+
+public interface Persoon {
+
+    // Functie voor het krijgen van de gegevens van de persoon
+    void getStudent();
+}
